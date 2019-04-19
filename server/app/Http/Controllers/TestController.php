cfe7ec84879bb7a85a8651621d30639d9910928d
pkmm <robotgg@126.com>
2019-04-20 02:26:43 +0800

    test redis

--- server/app/Http/Controllers/TestController.php
+++ server/app/Http/Controllers/TestController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Contracts\VerifyCodeRecognizeInterface;
 use App\Model\User;
+use App\Services\ZcmuEducationSystem;
+use Cache;
 use Illuminate\Http\Request;
 
 // 用于一些使用的
@@ -11,10 +13,20 @@ class TestController extends Controller
 {
     public function test(Request $request, VerifyCodeRecognizeInterface $verifyCodeRecognize)
     {
-        $userId = (int) $request->get('user_id', 1);
-        $user = User::find($userId);
-        $student = $user->student;
 
-        return $student->scores;
+
+        $userId = (int) $request->get('user_id', 2);
+        $redisKey = "userId:$userId:scores";
+        if (!($ret = Cache::get($redisKey))) {
+            $user = User::find($userId);
+            $student = $user->student;
+            if (!$student) {
+                return [];
+            }
+            $scores = $student->scores;
+            Cache::forever($redisKey, json_encode($scores));
+        }
+
+        return json_decode($ret);
     }
 }
