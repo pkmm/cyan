b603e561a776b3ee8b90299982edd0641818614e
pkmm <zccxxx79@gmail.com>
2019-04-30 10:56:32 +0800

    fix name error

--- client/src/pages/app.js
+++ client/src/pages/app.js
@@ -3,10 +3,10 @@ import Main from './layouts/main'
 import Login from "./login/login";
 import {inject, observer} from 'mobx-react'
 
-@inject('UserStore') @observer
+@inject('userStore') @observer
 export default class App extends Component {
   render() {
-    const {isLogin} = this.props.UserStore;
+    const {isLogin} = this.props.userStore;
     return isLogin ? <Main/> : <Login/>;
   }
 }
